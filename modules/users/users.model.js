72244e5535772b5caa7f72d25879e70dc97d6539
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 15:22:34 +0530

    created a user model

---
+++ modules/users/users.model.js
@@ -0,0 +1,117 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+import { toJSONPlugin } from "../../shared/plugins/toJSON.plugin.js";
+
+const { Schema } = mongoose;
+
+const ROLES = Object.freeze({
+  CANDIDATE: "candidate",
+  RECRUITER: "recruiter",
+});
+
+const AUTH_PROVIDERS = Object.freeze({
+  LOCAL: "local",
+  GOOGLE: "google",
+});
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: Object.values(ROLES),
+      required: true,
+      index: true,
+    },
+
+    avatar: {
+      url: {
+        type: String,
+        default: "",
+      },
+
+      publicId: {
+        type: String,
+        default: "",
+      },
+    },
+
+    authProvider: {
+      type: String,
+      enum: Object.values(AUTH_PROVIDERS),
+      default: AUTH_PROVIDERS.LOCAL,
+    },
+
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+      index: true,
+    },
+
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.index({ email: 1 }, { unique: true });
+
+userSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(12);
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.plugin(toJSONPlugin);
+
+export default mongoose.model("User", userSchema);
