5fd556493ba4bd427a8752d59fc6493ae9a5c352
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 15:46:05 +0530

    added shared certification schema

---
+++ shared/schemas/certification.schema.js
@@ -0,0 +1,26 @@
+import { Schema } from "mongoose";
+
+export const certificationSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    issuingOrganization: {
+      type: String,
+      required: true,
+    },
+
+    issueDate: Date,
+
+    expiryDate: Date,
+
+    credentialId: String,
+
+    credentialUrl: String,
+  },
+  {
+    _id: false,
+  },
+);
