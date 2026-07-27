ebb6e493dbdb1b5c1ba2fbfef1ee93383718c122
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 15:44:34 +0530

    added shared experience schema

---
+++ shared/schemas/experience.schema.js
@@ -0,0 +1,35 @@
+import { Schema } from "mongoose";
+
+export const experienceSchema = new Schema(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+
+    designation: {
+      type: String,
+      required: true,
+    },
+
+    employmentType: String,
+
+    location: String,
+
+    startDate: Date,
+
+    endDate: Date,
+
+    currentlyWorking: {
+      type: Boolean,
+      default: false,
+    },
+
+    description: String,
+
+    skills: [String],
+  },
+  {
+    _id: false,
+  }
+);
