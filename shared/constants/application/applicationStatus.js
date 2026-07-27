0223f639c2068a2891bd14b15bf00f073cf4cdcb
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 16:26:16 +0530

    added few more constants

---
+++ shared/constants/application/applicationStatus.js
@@ -0,0 +1,11 @@
+export const APPLICATION_STATUS = Object.freeze({
+  APPLIED: "Applied",
+  UNDER_REVIEW: "Under Review",
+  SHORTLISTED: "Shortlisted",
+  INTERVIEW_SCHEDULED: "Interview Scheduled",
+  INTERVIEWED: "Interviewed",
+  OFFERED: "Offered",
+  HIRED: "Hired",
+  REJECTED: "Rejected",
+  WITHDRAWN: "Withdrawn",
+});
---
+++ shared/constants/common/currencies.js
@@ -0,0 +1,6 @@
+export const CURRENCIES = Object.freeze({
+  INR: "INR",
+  USD: "USD",
+  EUR: "EUR",
+  GBP: "GBP",
+});
