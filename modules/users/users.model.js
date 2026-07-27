030e9be3214caacbdc029ab89fa339e564999ccb
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 21:49:41 +0530

    fixed the next is not a function error in userSchema

--- modules/users/users.model.js
+++ modules/users/users.model.js
@@ -96,14 +96,12 @@ userSchema.virtual("fullName").get(function () {
   return `${this.firstName} ${this.lastName}`;
 });
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(12);
 
   this.password = await bcrypt.hash(this.password, salt);
-
-  next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
