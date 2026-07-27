ccea81cf8c76a2bd657b46ce555d0ddfd935caa8
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 18:58:12 +0530

    added a validate middleware

---
+++ shared/middleware/validate.js
@@ -0,0 +1,19 @@
+import { ZodError } from "zod";
+import ApiError from "../utils/ApiError.js";
+
+const validate = (schema) => {
+  return (req, res, next) => {
+    try {
+      req.body = schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return next(new ApiError(400, "Validation failed", error.issues));
+      }
+
+      next(error);
+    }
+  };
+};
+
+export default validate;
