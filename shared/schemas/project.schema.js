c439a3c6d2e7826c9142eeb9f7734c9b3f562f5d
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 15:45:12 +0530

    added shared project schema

---
+++ shared/schemas/project.schema.js
@@ -0,0 +1,25 @@
+import { Schema } from "mongoose";
+
+export const projectSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: String,
+
+    technologies: [String],
+
+    projectUrl: String,
+
+    githubUrl: String,
+
+    startDate: Date,
+
+    endDate: Date,
+  },
+  {
+    _id: false,
+  },
+);
