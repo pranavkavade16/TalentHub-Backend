d0648e3cdfb89c3ff7316ac4c2c58cfc177748f7
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 16:08:04 +0530

    added company model

---
+++ modules/company/company.model.js
@@ -0,0 +1,90 @@
+import mongoose from "mongoose";
+
+import { fileSchema } from "../../shared/schemas/core/file.schema.js";
+import { addressSchema } from "../../shared/schemas/core/address.schema.js";
+import { contactSchema } from "../../shared/schemas/core/contact.schema.js";
+import { socialLinksSchema } from "../../shared/schemas/core/socialLinks.schema.js";
+
+import { COMPANY_SIZE } from "../../shared/constants/companySize.js";
+import { INDUSTRIES } from "../../shared/constants/industry.js";
+
+const { Schema } = mongoose;
+
+const companySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 100,
+    },
+
+    logo: fileSchema,
+
+    description: {
+      type: String,
+      maxlength: 3000,
+    },
+
+    industry: {
+      type: String,
+      enum: Object.values(INDUSTRIES),
+      required: true,
+      index: true,
+    },
+
+    companySize: {
+      type: String,
+      enum: Object.values(COMPANY_SIZE),
+      required: true,
+    },
+
+    foundedYear: {
+      type: Number,
+      min: 1800,
+      max: new Date().getFullYear(),
+    },
+
+    website: {
+      type: String,
+      trim: true,
+    },
+
+    address: addressSchema,
+
+    contact: contactSchema,
+
+    socialLinks: socialLinksSchema,
+
+    verified: {
+      type: Boolean,
+      default: false,
+      index: true,
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+      index: true,
+    },
+
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "RecruiterProfile",
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+companySchema.index({ name: "text" });
+
+companySchema.index({
+  industry: 1,
+  companySize: 1,
+});
+
+export default mongoose.model("Company", companySchema);
