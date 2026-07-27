a8d96a8d55ea28a782e676ae4ac06677b3b780c2
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-27 22:16:42 +0530

    created a login controller in auth controller

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -1,5 +1,5 @@
 import ApiResponse from "../../shared/utils/ApiResponse.js";
-import { registerUser } from "./auth.service.js";
+import { registerUser, loginUser } from "./auth.service.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -12,6 +12,34 @@ export const register = async (req, res, next) => {
         lastName: user.lastName,
         email: user.email,
         role: user.role,
+      }),
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { user, accessToken, refreshToken } = await loginUser(req.body);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json(
+      new ApiResponse(200, "Login successful.", {
+        accessToken,
+        user: {
+          id: user._id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          role: user.role,
+        },
       }),
     );
   } catch (error) {
