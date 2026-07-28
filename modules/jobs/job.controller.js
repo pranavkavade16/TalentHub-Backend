dadd1382819b44cde37f3c0a4c1880b9dcfe1388
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-28 17:36:25 +0530

    created a job controller file and create job method

---
+++ modules/jobs/job.controller.js
@@ -0,0 +1,16 @@
+import httpStatus from "http-status";
+
+import ApiResponse from "../../shared/utils/ApiResponse.js";
+import asyncHandler from "../../shared/utils/asyncHandler.js";
+
+import jobService from "./jobs.service.js";
+
+export const createJob = asyncHandler(async (req, res) => {
+  const job = await jobService.createJob(req.user._id, req.body);
+
+  return res
+    .status(httpStatus.CREATED)
+    .json(
+      new ApiResponse(httpStatus.CREATED, "Job created successfully.", job),
+    );
+});
