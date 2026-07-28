ed49690365f22b4af0fc01457c9bb044dfe33471
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-28 13:02:00 +0530

    created a logout route

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { validate } from "../../shared/middleware/validate.js";
 
-import { register, refresh, login } from "./auth.controller.js";
+import { register, refresh, login, logout } from "./auth.controller.js";
 import { loginSchema, registerUserSchema } from "./auth.validation.js";
 
 const router = Router();
@@ -12,5 +12,7 @@ router.post("/register", validate(registerUserSchema), register);
 router.post("/login", validate(loginSchema), login);
 
 router.post("/refresh", refresh);
+
+router.post("/logout", authenticate, logout);
 
 export default router;
