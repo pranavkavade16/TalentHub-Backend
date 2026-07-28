8f2af650ec6dc10745c6060ff61ea93869475888
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-28 18:11:54 +0530

    added a job srvice to fetch the jobs data for public and added sort options constants

--- modules/jobs/job.service.js
+++ modules/jobs/job.service.js
@@ -1,6 +1,7 @@
 import Job from "./jobs.model.js";
 import RecruiterProfile from "../recruiters/recruiterProfile.model.js";
-
+import { JOB_STATUS } from "../../shared/constants/job/jobStatus.js";
+import { SORT_OPTIONS } from "../../shared/constants/common/sortOptions.js";
 import ApiError from "../../shared/utils/ApiError.js";
 
 export const createJob = async (userId, jobData) => {
@@ -26,4 +27,103 @@ export const createJob = async (userId, jobData) => {
   });
 
   return job;
+};
+
+const getJobs = async (query) => {
+  const {
+    page,
+    limit,
+    search,
+    employmentType,
+    workplaceType,
+    experienceLevel,
+    city,
+    skills,
+    sort,
+  } = query;
+
+  const filter = {
+    status: JOB_STATUS.OPEN,
+    isDeleted: false,
+  };
+
+  if (search) {
+    filter.$text = {
+      $search: search,
+    };
+  }
+
+  if (employmentType) {
+    filter.employmentType = employmentType;
+  }
+
+  if (workplaceType) {
+    filter.workplaceType = workplaceType;
+  }
+
+  if (experienceLevel) {
+    filter.experienceLevel = experienceLevel;
+  }
+
+  if (city) {
+    filter["location.city"] = city;
+  }
+
+  if (skills) {
+    filter.skills = {
+      $in: skills.split(",").map((skill) => skill.trim().toLowerCase()),
+    };
+  }
+
+  const skip = (page - 1) * limit;
+
+  const sortOption = SORT_OPTIONS[sort] || SORT_OPTIONS.newest;
+
+  const queryBuilder = Job.find(filter)
+    .populate("company")
+    .populate({
+      path: "recruiter",
+      select: "designation",
+      populate: {
+        path: "user",
+        select: "firstName lastName profilePicture",
+      },
+    })
+    .sort(sortOption)
+    .skip(skip)
+    .limit(limit);
+
+  // Add text score only when using text search
+  if (search) {
+    queryBuilder.select({
+      score: {
+        $meta: "textScore",
+      },
+    });
+
+    queryBuilder.sort({
+      score: {
+        $meta: "textScore",
+      },
+    });
+  }
+
+  const [jobs, totalJobs] = await Promise.all([
+    queryBuilder,
+    Job.countDocuments(filter),
+  ]);
+
+  return {
+    jobs,
+    pagination: {
+      total: totalJobs,
+      page,
+      limit,
+      totalPages: Math.ceil(totalJobs / limit),
+    },
+  };
+};
+
+export default {
+  getJobs,
 };
---
+++ shared/constants/common/sortOptions.js
@@ -0,0 +1,6 @@
+export const SORT_OPTIONS = {
+  newest: { createdAt: -1 },
+  oldest: { createdAt: 1 },
+  titleAsc: { title: 1 },
+  titleDesc: { title: -1 },
+};
